be6a886832e1e2c31563940b4ecc06ef6cde3fff
ThijsHendrikx <thijs.hendrikx@icloud.com>
2015-05-11 12:24:42 +0200

    Removed screen orientation plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,20 +6,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/net.yoik.cordova.plugins.screenorientation/www/screenorientation.js",
-        "id": "net.yoik.cordova.plugins.screenorientation.screenorientation",
-        "clobbers": [
-            "cordova.plugins.screenorientation"
-        ]
-    },
-    {
-        "file": "plugins/net.yoik.cordova.plugins.screenorientation/www/screenorientation.android.js",
-        "id": "net.yoik.cordova.plugins.screenorientation.screenorientation.android",
-        "merges": [
-            "cordova.plugins.screenorientation"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
         "id": "org.apache.cordova.device-orientation.CompassError",
         "clobbers": [
@@ -45,7 +31,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.1-dev",
-    "net.yoik.cordova.plugins.screenorientation": "1.3.2",
     "org.apache.cordova.device-orientation": "0.3.11"
 }
 // BOTTOM OF METADATA
