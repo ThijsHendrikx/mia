e783da5846b349b563ffdecda11aba63e58b92a2
ThijsHendrikx <thijs.hendrikx@icloud.com>
2015-05-12 13:40:32 +0200

    Added first virtual reality

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,48 +1,80 @@
-var app = app || {};
- 
-app.watchID = null;
-app.dataElem = null;
- 
- 
+var app = app || {}
+
+
+app.translationLeft = null;
+app.translationRight = null;
+
+app.rotationLeft = null;
+app.rotationRight = null;
+
+app.prevRotations = [0,0,0];
+app.prevTranslations = [0,0,0];
+
+app.translationWidth = 0;
+
+
+
+
 app.initialize = function () {
     document.addEventListener('deviceready', app.onDeviceReady, false);
-};
+}
  
  
+
 app.onDeviceReady = function () {
 
+  app.rotationLeft  = document.getElementById("imgleft").querySelector(".rotationWrapper");
+  app.rotationRight = document.getElementById("imgright").querySelector(".rotationWrapper");
+
+  app.translationLeft  = app.rotationLeft.querySelector(".translationWrapper");
+  app.translationRight = app.rotationRight.querySelector(".translationWrapper");
+
+  app.translationWidth = app.translationLeft.offsetWidth;
+
   if (window.DeviceOrientationEvent) {
-    document.getElementById("doEvent").innerHTML = "DeviceOrientation";
-    // Listen for the deviceorientation event and handle the raw data
-    window.addEventListener('deviceorientation', function(eventData) {
-      // gamma is the left-to-right tilt in degrees, where right is positive
-      var tiltLR = eventData.gamma;
-
-      // beta is the front-to-back tilt in degrees, where front is positive
-      var tiltFB = eventData.beta;
-
-      // alpha is the compass direction the device is facing in degrees
-      var dir = eventData.alpha
-
-      // call our orientation event handler
-      //deviceOrientationHandler(tiltLR, tiltFB, dir);
-
-      document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-      document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-      document.getElementById("doDirection").innerHTML = Math.round(dir);
-
-      // Apply the transform to the image
-      var logo = document.getElementById("imgLogo");
-      logo.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
-      logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
-      logo.style.transform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
     
+    window.addEventListener('deviceorientation', function(eventData) {
+
+        var tiltLR = eventData.gamma;
+        var tiltFB = eventData.beta;
+        var dir = eventData.alpha;
+
+        app.prevRotations.shift();
+        app.prevRotations.push(tiltFB);
+
+        app.prevTranslations.shift();
+        app.prevTranslations.push(dir);
+
+        var normalizedRotation = (app.prevRotations[0] + app.prevRotations[1] + app.prevRotations[2]) / 3; 
+        var normalizedTranslation = (app.prevTranslations[0] + app.prevTranslations[1] + app.prevTranslations[2]) / 3; 
+
+        app.rotationLeft.style.webkitTransform = "rotate("+ -normalizedRotation +"deg)";
+        app.rotationRight.style.webkitTransform = "rotate("+ -normalizedRotation +"deg)";
+
+        app.translationLeft.style.left =  ((360 - Math.round(normalizedTranslation)) / 360) * -(app.translationWidth / 2) + "px";
+        app.translationRight.style.left = ((360 - Math.round(normalizedTranslation)) / 360) * -(app.translationWidth / 2) + "px";
+
+      //  app.showDebugInfo(tiltLR,tiltFB,((360 - Math.round(normalizedTranslation)) / 360));
+
     }, false);
   
   } else {
     document.getElementById("doEvent").innerHTML = "Not supported."
   }
      
-};
+}
+
+
+
+app.showDebugInfo = function(tiltLR,tiltFB,dir){
+
+  document.getElementById("debug").style.display = "block";
+
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = dir;
+
+}
+
 
  
