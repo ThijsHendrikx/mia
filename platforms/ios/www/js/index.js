a236b8628241157e4348bf3f297bf0b4744e3ffc
ThijsHendrikx <thijs.hendrikx@icloud.com>
2015-05-18 10:46:34 +0200

    added phonon integration

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,59 +1,79 @@
-var app = app || {};
- 
-app.watchID = null;
- 
- 
+var app = app || {}
+
+
+app.translationLeft = null;
+app.translationRight = null;
+
+app.rotationLeft = null;
+app.rotationRight = null;
+
+app.currentRotation = 0;
+app.currentTranslation = 0;
+
+app.translationWidth = 0;
+
+
+
+
 app.initialize = function () {
     document.addEventListener('deviceready', app.onDeviceReady, false);
-};
+}
  
  
+
 app.onDeviceReady = function () {
-    app.receivedEvent('deviceready');
-    app.watchID = navigator.compass.watchHeading(
-        app.compassUpdate, 
-        app.compassError, { frequency : 3000 });
 
-    alert(app.watchId);
-};
- 
- 
-app.compassUpdate = function (hdg) {
-  alert("update");
+  app.rotationLeft  = document.getElementById("imgleft").querySelector(".rotationWrapper");
+  app.rotationRight = document.getElementById("imgright").querySelector(".rotationWrapper");
 
-  var mh = hdg.magneticHeading;
-  app.showHeading(true, 'Heading: ' + mh);
-};
- 
- 
-app.compassError = function (err) {
-  alert("error");
+  app.translationLeft  = app.rotationLeft.querySelector(".translationWrapper");
+  app.translationRight = app.rotationRight.querySelector(".translationWrapper");
 
-  var errcode = err.code;
-  app.showHeading(false, 'Compass error: ' + errcode);
-};
- 
- 
-app.showHeading = function (f_ok, s) {
-  var parentElem = document.getElementById('heading');
-  var nodataElem = parentElem.querySelector('.listening');
-  var dataElem   = parentElem.querySelector('.received');
-  if (f_ok) { 
-    nodataElem.setAttribute('style', 'display:none;'); 
-    dataElem.setAttribute('style', 'display:block;'); 
-    dataElem.innerHTML = s;
+  app.translationWidth = app.translationLeft.offsetWidth;
+
+  if (window.DeviceOrientationEvent) {
+    
+    window.addEventListener('deviceorientation', function(eventData) {
+
+        var tiltLR = eventData.gamma;
+        var tiltFB = eventData.beta;
+        var dir = eventData.alpha;
+
+        var normalizedRotation = app.currentRotation = app.currentRotation + ( (tiltFB - app.currentRotation) * .1);
+        var normalizedTranslation = app.currentTranslation = app.currentTranslation + ( (dir - app.currentTranslation) * .1);
+
+        if( Math.abs(dir - app.currentTranslation) > 320){
+          normalizedTranslation = dir;
+        }
+
+        app.rotationLeft.style.webkitTransform = "rotate(" + - normalizedRotation + "deg)";
+        app.rotationRight.style.webkitTransform = "rotate("+ - normalizedRotation + "deg)";
+
+
+        app.translationLeft.style.left =  Math.round( ((360 - normalizedTranslation) / 360) * - (app.translationWidth / 2) ) + "px";
+        app.translationRight.style.left = Math.round( ((360 - normalizedTranslation) / 360) * - (app.translationWidth / 2) ) + "px";
+
+        app.showDebugInfo(tiltLR,tiltFB,dir);
+
+    }, false);
+  
+  } else {
+    document.getElementById("doEvent").innerHTML = "Not supported."
   }
-  else { 
-    nodataElem.setAttribute('style', 'display:block;'); 
-    dataElem.setAttribute('style', 'display:none;'); 
-    nodataElem.innerHTML = s;
-  }
-};
+     
+}
+
+
+
+app.showDebugInfo = function(tiltLR,tiltFB,dir){
+
+  document.getElementById("debug").style.display = "block";
+
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+}
+
+
  
- 
-app.receivedEvent = function(id) {
-    var parentElement = document.getElementById('deviceready');
-    parentElement.setAttribute('style', 'display:none;');
-    var headingElement = document.getElementById('heading');
-    headingElement.setAttribute('style', 'display:block');
-};
