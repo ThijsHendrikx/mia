946b4d49aa5b7b0097f8788669dfccc28631dd4d
ThijsHendrikx <thijs.hendrikx@icloud.com>
2015-05-18 11:40:16 +0200

    changed phonon integration and added test content

---
+++ www/js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var onDeviceReady = function () {
+  Phonon.Navigator().start('vrviewer');
+}
+
+var isCordova = typeof window.cordova !== 'undefined' ? true : false;
+
+if(isCordova) {
+  document.addEventListener('deviceready', onDeviceReady, false);
+
+} else {
+  window.addEventListener('load', onDeviceReady, false);
+}
+
+Phonon.Navigator({
+  defaultPage: 'vrviewer',
+  templatePath: 'tpl',
+  pageAnimations: true
+});
+
+
+Phonon.Navigator().on({page: 'vrviewer', template: 'vrviewer', asynchronous: false}, function(activity) {
+
+  activity.onCreate(function(self, el, req) {
+    console.log('vrviewer: onCreate');
+  });
+
+  activity.onReady(function(self, el, req) {
+    console.log('vrviewer: onReady');
+  });
+
+  activity.onTransitionEnd(function() {
+    console.log('vrviewer: onTransitionEnd');
+  });
+
+  activity.onQuit(function(self) {
+    console.log('vrviewer: onQuit');
+  });
+
+  activity.onHidden(function(el) {
+    console.log('vrviewer: onHidden');
+  });
+});
+
